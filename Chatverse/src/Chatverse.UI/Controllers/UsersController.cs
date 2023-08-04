b29bdf3efcc12da9616e6ebe78d2b2cd0bef748d
Nihad Soltanov <mr.nihadsoltanov@gmail.com>
2023-08-04 15:55:56 +0400

    personal profile ui

---
+++ Chatverse/src/Chatverse.UI/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chatverse.UI.Controllers
+{
+    public class UsersController : Controller
+    {
+       public async Task<IActionResult> AuthorProfile()
+        {
+            return View();
+        }
+    }
+}
