17b85286de189c72915efd522a66306b3d834fbf
Nihad Soltanov <mr.nihadsoltanov@gmail.com>
2023-08-04 12:55:48 +0400

    posts list by friend in ui

--- Chatverse/src/Chatverse.UI/Controllers/MainController.cs
+++ Chatverse/src/Chatverse.UI/Controllers/MainController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using Chatverse.UI.ViewModels.Post;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Chatverse.UI.Controllers
 {
@@ -12,7 +14,15 @@ namespace Chatverse.UI.Controllers
             var accessToken = HttpContext.Session.GetString("JWToken");
             if (accessToken == null) return RedirectToAction("Login", "Auth");
             HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);        
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            HttpResponseMessage response = await client.GetAsync($"{baseUrl}/Posts/GetPostsByFriend");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync(); // JSON yanıtı string olarak al
+
+                List<GetPostsByFriendsViewModel> posts = JsonConvert.DeserializeObject<List<GetPostsByFriendsViewModel>>(jsonResponse); // JSON'u List<string> olarak deserialize et
+                return View(model: posts);
+            }
             return View();
         }
     }
