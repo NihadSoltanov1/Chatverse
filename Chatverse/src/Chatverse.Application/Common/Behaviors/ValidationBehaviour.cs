5815df624137dfa17715450d16d4919c617ccd89
Nihad Soltanov <mr.nihadsoltanov@gmail.com>
2023-08-12 16:42:02 +0400

    solve validation bugs

--- Chatverse/src/Chatverse.Application/Common/Behaviors/ValidationBehaviour.cs
+++ Chatverse/src/Chatverse.Application/Common/Behaviors/ValidationBehaviour.cs
@@ -4,30 +4,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chatverse.Application.Common.Behaviors
 {
-    //public class ValidationBehaviour<TRequest, TResult> : IPipelineBehavior<TRequest, TResult> where TRequest : class
-    //{
-    //    private readonly IEnumerable<IValidator<TRequest>> _validators;
+    public class ValidationBehaviour<TRequest, TResult> : IPipelineBehavior<TRequest, TResult> where TRequest : IRequest<TResult>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-    //    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
-    //    {
-    //        _validators = validators;
-    //    }
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
 
-    //    public async Task<TResult> Handle(TRequest request, RequestHandlerDelegate<TResult> next, CancellationToken cancellationToken)
-    //    {
-    //        var ctx = new ValidationContext<TRequest>(request);
-    //        var validationResult = _validators
-    //        .Select(v => v.Validate(ctx))
-    //        .SelectMany(v => v.Errors)
-    //        .Where(x => x is not null)
-    //        .ToList();
-    //        if (validationResult.Any())
-    //            throw new ValidationException(validationResult);
-    //        return await next();
-    //    }
-    //}
+     
+
+        public async Task<TResult> Handle(TRequest request, RequestHandlerDelegate<TResult> next, CancellationToken cancellationToken)
+        {
+            var ctx = new ValidationContext<TRequest>(request);
+            var validationResult = _validators
+            .Select(v => v.Validate(ctx))
+            .SelectMany(v => v.Errors)
+            .Where(x => x is not null)
+            .ToList();
+            if (validationResult.Any())
+                throw new ValidationException(validationResult);
+            return await next();
+        }
+    }
 }
--- Chatverse/src/Chatverse.Application/ConfigureService.cs
+++ Chatverse/src/Chatverse.Application/ConfigureService.cs
@@ -4,15 +4,24 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-namespace Chatverse.Application
+using Chatverse.Application.Common.Behaviors;
+
+namespace Chatverse.Application 
 {
     public static class ConfigureServices
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()); // minimun setup
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+                // cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+                // cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
+                // cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+            });
             services.AddFluentValidationAutoValidation();
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-            services.AddMediatR(Assembly.GetExecutingAssembly());
             return services;
         }
 
