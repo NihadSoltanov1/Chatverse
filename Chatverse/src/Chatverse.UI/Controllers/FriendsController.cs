5574a4ffe9737adffc77f8b0ec3bc5e3ffa3e324
Nihad Soltanov <mr.nihadsoltanov@gmail.com>
2023-08-13 14:47:29 +0400

    find friend ui ready

---
+++ Chatverse/src/Chatverse.UI/Controllers/FriendsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chatverse.UI.Controllers
+{
+    public class FriendsController : Controller
+    {
+        public IActionResult FindFriend()
+        {
+            return View();
+        }
+    }
+}
